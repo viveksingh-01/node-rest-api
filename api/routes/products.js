552ef1cb045023784c485a0f6f7daf5e0ac945cd
viveksingh-01 <viveksingh9301@gmail.com>
2020-07-11 22:55:20 +0530

    Add POST, PATCH and DELETE requests to products route

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,7 +4,33 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.status(200).json({
-    message: 'Handling requests to /products',
+    message: 'Products were fetched.'
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Product with id ${id} was fetched.`
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(201).json({
+    message: 'Product was created.'
+  });
+});
+
+router.patch('/:id', (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).json({
+    message: `Product with id ${id} was updated.`
+  });
+});
+
+router.delete('/:id', (req, res, next) => {
+  res.status(200).json({
+    message: 'Product was deleted.'
   });
 });
 
