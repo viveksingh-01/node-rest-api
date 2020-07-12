b953a274fbfc5848541fc8acf97c813ebe47b583
viveksingh-01 <viveksingh9301@gmail.com>
2020-07-12 23:15:30 +0530

    Implement error handling

--- app.js
+++ app.js
@@ -5,9 +5,28 @@ const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+// Middleware used for logging
 app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
+
+
+// Handle errors related to invalid routes
+app.use((req, res, next) => {
+  const error = new Error('Not found.');
+  error.status = 404;
+  next(error);
+});
+
+
+// Handle any error which occurs in the application
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
