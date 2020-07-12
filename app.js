e89b84c3f17ab048d1f817b93809a0a7d0838300
viveksingh-01 <viveksingh9301@gmail.com>
2020-07-12 22:59:30 +0530

    Use morgan module to implement logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
