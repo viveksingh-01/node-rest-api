53a3edb3e015c32efcac8dd7945d301b7ac1f47c
viveksingh-01 <viveksingh9301@gmail.com>
2020-07-16 08:15:15 +0530

    Add product model

---
+++ api/models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
