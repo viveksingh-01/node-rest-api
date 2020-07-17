2421faedfa06df06e014cb3ac10ff37977848d00
viveksingh-01 <viveksingh9301@gmail.com>
2020-07-17 22:24:14 +0530

    Implement DB operations in http methods in products

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,43 +1,101 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Products were fetched.'
-  });
+router.get('/', (req, res) => {
+  Product.find()
+    .then(products => {
+      res.status(200).json({
+        message: 'Products were fetched successfully.',
+        products
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: {
+          message: 'Something went wrong.',
+          error
+        }
+      });
+    });
 });
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', (req, res) => {
   const id = req.params.id;
-  res.status(200).json({
-    message: `Product with id ${id} was fetched.`
-  });
+  Product.findById({ _id: id })
+    .then(product => {
+      if (product) {
+        res.status(200).json({
+          message: 'Product was fetched successfully.',
+          product
+        });
+      } else {
+        res.status(404).json({
+          message: 'No record found.'
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: {
+          message: 'Something went wrong.',
+          error
+        }
+      });
+    });
 });
 
-router.post('/', (req, res, next) => {
-  const product = {
-    name: req.body.name,
-    price: req.body.price
-  };
+router.post('/', (req, res) => {
+  const products = [];
+  for (let item of req.body) {
+    const product = new Product({
+      _id: new mongoose.Types.ObjectId(),
+      name: item.name,
+      price: item.price
+    });
+    products.push(product);
+  }
 
-  res.status(201).json({
-    message: 'Product was created.',
-    product: product
-  });
+  Product.insertMany(products)
+    .then(product => {
+      res.status(201).json({
+        message: 'Product was added successfully.',
+        product
+      });
+    })
+    .catch(error => console.log(error));
 });
 
-router.patch('/:id', (req, res, next) => {
+router.patch('/:id', (req, res) => {
   const id = req.params.id;
-  res.status(200).json({
-    message: `Product with id ${id} was updated.`
-  });
+  Product.update(
+    { _id: id },
+    {
+      $set: {
+        name: req.body.name,
+        price: req.body.price
+      }
+    }
+  )
+    .then(() => {
+      res.status(200).json({
+        message: 'Product was updated successfully.'
+      });
+    })
+    .catch(error => console.log(error));
 });
 
-router.delete('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'Product was deleted.'
-  });
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  Product.deleteOne({ _id: id })
+    .then(() => {
+      res.status(200).json({
+        message: 'Product was deleted successfully.'
+      });
+    })
+    .catch(error => console.log(error));
 });
 
 module.exports = router;
