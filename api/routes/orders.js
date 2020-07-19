90cfe2b1fe68de6cd67578f8611b157f5b5f579d
viveksingh-01 <viveksingh9301@gmail.com>
2020-07-19 12:43:56 +0530

    Implement DB operations for order routes

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,40 +1,94 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders were fetched.'
-  });
+  Order.find()
+    .select({ productId: 1, quantity: 1 })
+    .then(orders => {
+      res.status(200).json({
+        message: 'Orders fetched successfully.',
+        orders
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Something went wrong.',
+        error
+      });
+    });
 });
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  res.status(200).json({
-    message: `Order with id ${id} was fetched.`
-  });
+  Order.findById({ _id: id })
+    .select({ productId: 1, quantity: 1 })
+    .then(order => {
+      if (!order) {
+        return res.status(404).json({
+          message: 'No record found'
+        });
+      }
+      res.status(200).json({
+        message: 'Order fetched successfully.',
+        order
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Something went wrong',
+        error
+      });
+    });
 });
 
-router.post('/', (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  };
-  res.status(201).json({
-    message: 'Order was created.',
-    order: order
-  });
+router.post('/', (req, res) => {
+  const { productId, quantity } = req.body;
+  Product.findById({ _id: productId })
+    .then(product => {
+      if (!product) {
+        return res.status(404).json({
+          message: 'Product not found.'
+        });
+      }
+      const order = new Order({ _id: new mongoose.Types.ObjectId(), productId, quantity });
+      order
+        .save()
+        .then(order => {
+          res.status(201).json({
+            message: 'Order was placed successfully!',
+            order
+          });
+        })
+        .catch(error => {
+          res.status(500).json({
+            message: 'Something went wrong.',
+            error
+          });
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Something went wrong.',
+        error
+      });
+    });
 });
 
-router.patch('/:id', (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).json({
-    message: `Order with id ${id} was updated.`
-  });
-});
-
-router.delete('/:id', (req, res, next) => {
-  res.status(200).json({
-    message: 'Order was deleted.'
-  });
+router.delete('/:id', (req, res) => {
+  Order.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({
+        message: 'Order was deleted successfully.'
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'Something went wrong.',
+        error
+      });
+    });
 });
 
 module.exports = router;
