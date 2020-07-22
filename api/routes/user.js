c2aa1da75c4e10075dc9bd1d9f72b0731a2e9638
viveksingh-01 <viveksingh9301@gmail.com>
2020-07-22 22:00:08 +0530

    Add email validation for existing user

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,35 +4,31 @@ const bcrypt = require('bcryptjs');
 const router = express.Router();
 const User = require('../models/user');
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
   const { email, password } = req.body;
-  bcrypt
-    .hash(password, 10)
-    .then(hashedPassword => {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email,
-        password: hashedPassword
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        message: 'User already registered. Please login!'
       });
-      user
-        .save()
-        .then(result => {
-          res.status(200).json({
-            message: 'Signup successful'
-          });
-        })
-        .catch(error => {
-          res.status(500).json({
-            message: 'Something went wrong',
-            error
-          });
-        });
-    })
-    .catch(error => {
-      return res.status(500).json({
-        error
-      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      email,
+      password: hashedPassword
     });
+    await user.save();
+    res.status(200).json({
+      message: 'Signup successful'
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Something went wrong.',
+      error
+    });
+  }
 });
 
 module.exports = router;
